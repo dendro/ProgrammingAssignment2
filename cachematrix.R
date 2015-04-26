396c7c35c4c52f31f3fae09f649abb90a7b49884
dendro <dendro@user.noreply.github.com>
2015-04-26 22:20:09 +0200

    implemented makeCacheMatrix and cacheSolve with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the Inverse of an invertible Matrix in order to avoid repeated 
+## computation using a pair of functions
+## Example:
+## mm = matrix(c(4,3,3,2),2)
+## mcm <- makeCacheMatrix(mm)
+## cacheSolve(mcm)
 
-## Write a short comment describing this function
+## Create an object which allows to store a matrix and its inverse using getters
+## and setters
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL  # initialize object for inversed matrix
+    set <- function(y) {  # assign a value to an object in global environment
+        x <<- y  # change x (this function's parameter!) in global environment
+        i <<- NULL  # initialize inversed matrix in global environment
+    }
+    get <- function() x  # return the matrix
+    setinverse <- function(solve) i <<- solve  # calculate inverse matrix
+    getinverse <- function() i  # return inversed matrix
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)  # wrap our special "matrix" as a list
 }
 
 
-## Write a short comment describing this function
+## using the function makeCacheMatrix, calculate the inverse of a given matrix
+## either by actually performing the calculation or retrieve the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {  # check whether inverse was already calculated (cached)
+        message("getting cached data")
+        return(i)  # the cached inversed matrix
+    }
+    ## as the inverse has not yet been calculated (cached), perform caculation
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)  # cache the inversed matrix 
+    i  # Return a matrix that is the inverse of 'x'
 }
